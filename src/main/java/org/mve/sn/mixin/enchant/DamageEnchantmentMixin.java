fabda1b31d9b1fb351c9878064fb1fb1ee7081d9
MeiVinEight <MeiVinEight@outlook.com>
2025-01-01 14:01:30 +0800

    Widen enchantments compatibility and max level

---
+++ src/main/java/org/mve/sn/mixin/enchant/DamageEnchantmentMixin.java
@@ -0,0 +1,18 @@
+package org.mve.sn.mixin.enchant;
+
+import net.minecraft.enchantment.DamageEnchantment;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DamageEnchantment.class)
+public class DamageEnchantmentMixin
+{
+	@Inject(at = @At("RETURN"), method = "Lnet/minecraft/enchantment/DamageEnchantment;getMaxLevel()I", cancellable = true)
+	public void getMaxLevel(CallbackInfoReturnable<Integer> cir)
+	{
+		DamageEnchantment _this = (DamageEnchantment)(Object)this;
+		if (_this.typeIndex == 0) cir.setReturnValue(10);
+	}
+}
---
+++ src/main/java/org/mve/sn/mixin/enchant/EnchantmentMixin.java
@@ -0,0 +1,44 @@
+package org.mve.sn.mixin.enchant;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
+import net.minecraft.enchantment.LuckEnchantment;
+import net.minecraft.item.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Enchantment.class)
+public class EnchantmentMixin
+{
+	/**
+	 * @author MeiVinEight
+	 * @reason Widen enchantment combination
+	 */
+	@Overwrite
+	public final boolean canCombine(Enchantment other)
+	{
+		return (Object) this != other;
+	}
+
+	@Inject(at = @At("RETURN"), method = "isAcceptableItem(Lnet/minecraft/item/ItemStack;)Z", cancellable = true)
+	public void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir)
+	{
+		Enchantment _this = (Enchantment) (Object) this;
+		if (_this instanceof LuckEnchantment luck)
+		{
+			if (luck.target == EnchantmentTarget.WEAPON)
+			{
+				boolean flag = cir.getReturnValue();
+				flag |= stack.getItem() == Items.BOW;
+				flag |= stack.getItem() == Items.CROSSBOW;
+				flag |= stack.getItem() instanceof AxeItem;
+				flag |= stack.getItem() == Items.TRIDENT;
+				flag |= stack.getItem() instanceof SwordItem;
+				cir.setReturnValue(flag);
+			}
+		}
+	}
+}
